b930175673a9ae81ddfea42304826229df863941
Mick Muzac <mmuzac23@gmail.com>
2014-06-11 21:56:43 -0400

    Added intial connection support

--- src/ReflexServer.java
+++ src/ReflexServer.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 import com.impressflow.net.Account;
+import com.impressflow.net.ConnectionRequest;
 import com.impressflow.net.Status;
 
 
@@ -22,12 +24,12 @@ public class ReflexServer {
 	    server.getKryo().register(String[].class);
 	    server.getKryo().register(Account.class);
 	    server.getKryo().register(Status.class);
+	    server.getKryo().register(ConnectionRequest.class);
 	    
 	    server.start();
 	    try {
 			server.bind(3000);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	    
@@ -36,26 +38,57 @@ public class ReflexServer {
 	    	@Override
 	        public void received (Connection connection, Object object) {
 	    		  
+	              if(object instanceof Status){
+	            	  //If we get a status, blindly forward it
+	            	  Status temp = (Status) object;
+	            	  server.sendToTCP(temp.toId, temp);
+	              }
+	    		
 	    		  //Registering new account!
-	              if(object instanceof Account){
+	              else if(object instanceof Account){
 	            	  Account temp = (Account) object;
-	            	  System.out.println("Registering new account: " + temp.name);
+	            	  System.out.println("Registering new account: " + temp.emails);
 	            	  
 	            	  accounts.put(connection.getID(), temp);
-	            	  connection.sendTCP("account registered: " + temp.name + " " + connection.getID());
+	            	  connection.sendTCP("account registered: " + temp.emails + " " + connection.getID());
 	              }
 	              
-	              if(object instanceof Status){
-	            	  Status temp = (Status) object;
+	              else if(object instanceof ConnectionRequest){
+	            	  ConnectionRequest temp = (ConnectionRequest) object;
 	            	  
-	            	  server.sendToTCP(1, temp);
+	            	  if(temp.random == true){
+	            		  Set<Integer> keys = accounts.keySet();
+	            		  for(Integer key : keys){
+	            			  Account account = accounts.get(key);
+	            			  
+	            			  //If this account is not connected AND it is not the account associated with this connection
+	            			  //AND the user must want to connect randomly
+	            			  if(!account.isConnected && !key.equals(connection.getID())){
+	            				  account.isConnected = true;
+	            				  account.toId = connection.getID();
+	            				  
+	            				  //Do the opposite for the other connection
+	            				  account = accounts.get(connection.getID());
+	            				  account.isConnected = true;
+	            				  account.toId = key;
+	            				  
+	            				  server.sendToTCP(connection.getID(), new Status());
+	            				  return;
+	            			  }
+	            		  }
+	            	  }
+	            	  
+	            	  else{}
 	              }
+
 
 	        }
 	    	
 	    	@Override
 	    	public void disconnected(Connection connection){
 	    		accounts.remove(connection.getID());
+	    		
+	    		//To-do: Disconnect the account that was connected to this one, if it exists...
 	    	}
 	     });
 	}
